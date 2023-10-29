880bfe1d8affb298663c15b8d49ae04f0535c5dc
Msg-Lbo <24130801@qq.com>
2023-10-29 16:16:31 +0800

    更新

--- server/db/index.js
+++ server/db/index.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2/promise');
 
 // 创建数据库连接池
 const pool = mysql.createPool({
-    host: 'localhost',
+    host: '127.0.0.1',
     user: 'blog',
     password: '141444',
     database: 'blog',
